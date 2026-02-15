b28f42d9023d0e27536d0abe5fc8225a903f732f
kevin git <ahlinjuniorkevincatarya@gmail.com>
2026-02-15 02:20:36 +0000

    UI amelioration

--- main_IDE.py
+++ main_IDE.py
@@ -7,15 +7,27 @@ root = Tk()
 root.title("MinIDE")
 root.geometry("500x400")
 
+style = ttk.Style()
+style.theme_use('clam')  # thème moderne
+style.configure("TNotebook.Tab",
+                padding=[12, 8],
+                font=('Arial', 11, 'bold'),
+                background="#FFD700",   # doré
+                foreground="#000000",
+                relief="flat")   # texte noir pour contraste
+style.map("TNotebook.Tab",
+          background=[("selected", "#FFA500")],  # onglet actif : orange doré
+          foreground=[("selected", "#000000")])
+style.configure("TNotebook", background="#222222",borderwidth=0)  # fond du Notebook (gris foncé/noir)
 notebook = ttk.Notebook(root)
-notebook.pack(fill="both", expand=True)
+notebook.pack(fill="both", expand=True,padx=5,pady=5)
+my_font = font.Font(family="Consolas", size=12)
 
 
 def newTab():
-    frame = Frame(notebook)
-    my_font = font.Font(family="Arial", size=10, weight="normal")
-    txt = Text(frame, font=my_font)
-    txt.pack(fill="both", expand=True)
+    frame = Frame(notebook,bg="#1e1e1e")
+    txt = Text(frame, font=my_font,bg="#1e1e1e",fg="#FFFFFF",insertbackground="#FFD700",wrap="word", undo=True,relief=FLAT, bd=0)
+    txt.pack(fill="both", expand=True,padx=5,pady=5)
     notebook.add(frame,text=f"Tab {len(notebook.tabs())+1}")
     notebook.select(frame)
     txt.focus_set()
@@ -70,9 +82,9 @@ def pastetxt():
     
     
 
-menubar = Menu(root)
+menubar = Menu(root, bg="#1e1e1e", fg="#FFFFFF", activebackground="#333333", activeforeground="#FFFFFF",relief=FLAT)
 #file menu
-fmenu = Menu(menubar,tearoff=0)
+fmenu = Menu(menubar, tearoff=0, bg="#1e1e1e", fg="#FFFFFF", activebackground="#333333", activeforeground="#FFFFFF")
 fmenu.add_command(label="New", command = newTab)
 fmenu.add_command(label="Save",command= savefile)
 fmenu.add_command(label="Open", command= openfile)
@@ -80,7 +92,7 @@ fmenu.add_separator()
 fmenu.add_cascade(label="Exit",command=root.quit)
 menubar.add_cascade(label="File",menu=fmenu)
 #edit menu
-emenu = Menu(menubar, tearoff=0)
+emenu = Menu(menubar, tearoff=0, bg="#1e1e1e", fg="#FFFFFF", activebackground="#333333", activeforeground="#FFFFFF")
 emenu.add_command(label="Copy", command=copytxt)
 emenu.add_command(label="Paste", command=pastetxt)
 menubar.add_cascade(label="Edit",menu=emenu)
